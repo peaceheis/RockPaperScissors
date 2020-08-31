47d5054c29f6e71e6b5c2cf3995b3b9873fee9c2
peaceheis <69596656+peaceheis@users.noreply.github.com>
2020-08-31 15:52:28 -0500

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,94 +1,89 @@
 
 #Boilerplate
 import time
-import sys
 import random
+import sys
 choices = ["rock", "paper", "scissors"]
 valid = ["rock", "paper", "scissors", "stop"]
 outcomes = ["draws!", "loses!", "wins!"]
-sad_replies = ["That was... quick.", "Bye. :(", "See you..."]
-computer_count = 0
-player_count = 0
 player_input = ""
-player_equivalent = 0
-computer_input = 0
-computer_equivalent = ""
+computer_score = 0
+player_score = 0
 
-#function for deciding computer decision and randomized time for decision
-def computer_initiate() :
-  for i in range(random.randrange(1,4)) :
-    print("Computer: calculating ", end = "")
-    for i in range(3) :
-      print(".", end = '', flush = "true")
-      time.sleep(2)
-    time.sleep(random.randrange(0,4))
-    print("")
-  time.sleep(3)
-  computer_input = random.randrange(0,3)
-  print("Computer has decided!")
+#FUNCTION DEFINING TIME!
 
-#search choices for player's input and return index
-def search_choices(input):
-    for i in range (len(choices)):
-        if choices[i] == input:
-            return i
-    return -1
+#CHECKS PLAYER INPUT IS VALID
+def check_valid(player_inp) :
+    while(player_inp.lower() not in valid) :
+        player_inp = input("Please enter 'rock', 'paper', 'scissors', or 'stop'. ")
+    return player_inp
+    
+#CHECKS IF PLAYER SAID 'STOP'
+def check_stop(player_inp) :
+    if (player_inp.lower() == "stop") :
+        time.sleep(1.5)
+        print("See ya!")
+        return True
+        sys.exit()
 
-#determines winner of round and updates scores
-def player_results(player_equivalent, computer_input, countc, countp) :
-  if ((computer_input - player_equivalent) % 3) == 1 :
-    countc = countc + 1
-  elif ((computer_input - player_equivalent) % 3) == 2 :
-    countp = countp + 1
-  print("Player " + outcomes[(computer_input - player_input) % 3])
+#CREATES COMPUTER CHOICE
+def computer_choice_maker() :
+    computer_choice = random.randrange(0,3)
+    print("Computer has decided!")
+    return computer_choice
 
-#Prints scores
-  def print_scores() : 
-    print("Player's score: " + player_count)
-    print("Computer's score :" + computer_count)
+#CONVERTS PLAYER INPUT INTO PROGRAM-READABLE FORMAT
+def player_convert(player_inp) :
+    return choices.index(player_inp)
 
-#converts computer's number input to string
-def computer_convert(computer) :
-  return choices[computer]
-#First Round
+#DECIDES WHO WON
+def decisions(computer_choice, player_choice) :
+    return outcomes[(computer_choice - player_choice) %3]
 
+#SCOREBOARD COUNTER
+def scoreboard(computer_choice, player_choice) :
+    global computer_score
+    global player_score
+    did_lose = ((computer_choice - player_choice) %3 ==1)
+    did_win = ((computer_choice - player_choice) %3 ==2)
+    if did_lose :
+        computer_score
+        computer_score+= 1
+    if did_win :
+        player_score
+        player_score += 1
+ 
+    
+# </FUNCTION DEFINING TIME>
+
+#INTRO TO GAMEPLAY
 print("Let's play Rock, Paper, Scissors!")
 time.sleep(1)
-print("To stop, say 'stop' at any time.")
-print("Let's start!")
-computer_initiate()
-player_input = input("Player, decide: ")
-
-#check player input
-while player_input.lower() not in valid :
-    player_input = input("Please say 'rock', 'paper', 'scissors', or 'stop'.")
-
-#If player immediately says stop, give a sad reply:
-if player_input.lower() == "stop" :
-  sys.exit(sad_replies[random.randrange(0,2)])
-
-#Logic to determine winner
-def decisions() : 
-  player_equivalent = search_choices(player_input)
-  computer_equivalent = computer_convert(computer_input)
-  print("Computer chose: " + computer_equivalent)
-  player_results(player_equivalent, computer_input, computer_count, player_count)
-
-decisions()
-#Rest of Game
-while (player_input != "stop") :
-  computer_initiate()
-  player_input = input("Player, decide: ")
-  while player_input.lower() not in valid :
-    player_input = input("Please say 'rock', 'paper', 'scissors', or'stop'")
-  if player_input.lower() == "stop" :
-    break
-  decisions()
-  
-
-time.sleep(1)
-sys.exit("See ya!")
-
-
-
- 
+print("To stop the game, say 'stop' at any time!")
+time.sleep(1.5)
+#GAMEPLAY LOOP
+while("true" == "true") :
+     print("New Round!")
+     time.sleep(1)
+     computer_var = computer_choice_maker()
+     player_input = input("Player choice: ")
+     player_input = check_valid(player_input)
+     check_stop(player_input)
+     time.sleep(1)
+     player_number = player_convert(player_input)
+     print("Computer chose " + choices[computer_var] + "!")
+     time.sleep(1)
+     print("Player " + decisions(computer_var, player_number))
+     time.sleep(2)
+     scoreboard(computer_var, player_number)
+     time.sleep(1)
+     print("Player score : " ,player_score)
+     time.sleep(1)
+     print("Computer score : " ,computer_score)
+     time.sleep(3)
+     print("")
+     print("")
+     
+     
+     
+        
